ac9c37abdeabf6c1cf6e6dc298afc9de5673aac9
Yuzuno <129194956+Yuzuno@users.noreply.github.com>
2023-04-04 08:18:50 +0200

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "game.h"
+
+using std::cout;
+using std::endl;
+
+/*NAMING SYNTAX:
+
+constructor xxx of class xxx:       cls_xxx
+struct xxx:                         stc_xxx
+class xxx:                          cls_xxx
+object xxx:                         obj_xxx
+int array xxx:                      arr_int_xxx
+int fct xxx:                        fct_int_xxx
+set int xxx:                        set_int_xxx
+get int xxx:                        get_int_xxx
+int pointer xxx:                    ptr_int_xxx
+
+string var xxx:                     str_xxx
+int var xxx:                        int_xxx
+unsigned int var xxx:               uint_xxx
+const str var xxx:                  STR_xxx
+const int var xxx:                  INT_xxx
+const uint var xxx:                 UINT_xxx
+
+fct int arg xxx:                    int_aXxx
+*/
+
+
+
+int main()
+{
+
+	fct_void_main_menu();
+
+
+	fct_void_character_choice();
+
+	cout << "You awaken in a plain, without any memory of what happened before nor who you are. However, you feel something is wrong as if you didn't belong to where you're at. You see in the distance a farm,a long road and a river" << endl;
+
+	system("pause");
+
+	fct_void_direction_choice();
+
+
+
+}
